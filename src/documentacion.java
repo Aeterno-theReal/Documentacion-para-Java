e149490b324ae366066ec68a2de48e75d18a8333
David Pérez <aeterno1@hotmail.com>
2019-08-05 10:10:52 +0200

    Documentacion, inicial

---
+++ src/documentacion.java
@@ -0,0 +1,31 @@
+
+public class documentacion {
+
+	/**
+	 * Funcion de prueba para probar la documentación.
+	 * @param x Primer parámetro para la operación
+	 * @param y Segundo parámetro para la operación
+	 * @return La operación realizada
+	 * @see MainClass#Mostrar
+	 * @see Scanner
+	 */
+	public static int miFuncion(int x, int y)
+	{
+		return 0;
+	}
+	
+	/**
+	 * Documentación de variable global.
+	 */
+	public static int variableGlobal;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		variableGlobal=0;
+		
+		variableGlobal=miFuncion(variableGlobal, variableGlobal);
+		
+	}
+
+}
